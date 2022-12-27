13c9b53ea3919ef81feb61033a607aa9af635299
Joseph Karnafel <joseph@riff.agency>
2022-12-27 11:58:45 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Play Nine`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
